136f6c67baa75711125fe79660d63a4ecd2cf172
Nhyu <kruiz1995@hotmail.com>
2024-08-14 01:08:24 -0300

    feat: ABM Instalacion

--- Models/Reservas/InstalacionEstado.cs
+++ Models/Reservas/InstalacionEstado.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string DescripcionEstado { get; set; }
-        public string NombreEstado { get; set; } // Activo, Inactivo, Abierta, CerradaReparacion, CerradaRemodelacion
+        public string NombreEstado { get; set; } // Activo, Inactivo, Abierta, Cerrada, CerradaMantenimiento
         public DateTime FechaCreacion { get; set; }
         public DateTime? FechaBaja { get; set; }
         public DateTime? FechaModificacion { get; set; }
---
+++ Services/InstalacionServices.cs
@@ -0,0 +1,86 @@
+﻿using ApiNet8.Data;
+using ApiNet8.Models.DTO;
+using ApiNet8.Models.Reservas;
+using ApiNet8.Services.IServices;
+using AutoMapper;
+using XAct.Library.Settings;
+
+namespace ApiNet8.Services
+{
+    public class InstalacionServices : IInstalacionServices
+    {
+        private readonly ApplicationDbContext _db;
+        private string secretToken;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public InstalacionServices(ApplicationDbContext db, IConfiguration configuration, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            this._db = db;
+            this.secretToken = configuration.GetValue<string>("ApiSettings:secretToken") ?? "";
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        Instalacion IInstalacionServices.ActualizarInstalacion(InstalacionDTO instalacionDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        Instalacion IInstalacionServices.CrearInstalacion(InstalacionDTO instalacionDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        Instalacion IInstalacionServices.EliminarInstalacion(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IInstalacionServices.ExisteInstalacion(string nombre)
+        {
+            throw new NotImplementedException();
+        }
+
+        Instalacion IInstalacionServices.GetInstalacionById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Instalacion> IInstalacionServices.GetInstalaciones()
+        {
+            try
+            {
+                return _db.Instalacion.ToList();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+        }
+
+        List<Instalacion> IInstalacionServices.GetInstalacionesActivas()
+        {
+            try
+            {
+                List<Instalacion> listComp = _db.Instalacion.ToList();
+                List<Instalacion> listFiltrada = new List<Instalacion>();
+
+                //foreach (var instalacion in listComp)
+                //{
+                //    InstalacionHistorial estado = _db.InstalacionHistorial.Where(i => i.InstalacionEstado.Id != 2 && i.FechaFin != null).FirstOrDefault();
+                //    if (estado != null)
+                //    {
+                //        listFiltrada.Add(instalacion);
+                //    }
+                //}
+
+                return listFiltrada;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+        }
+    }
+}
